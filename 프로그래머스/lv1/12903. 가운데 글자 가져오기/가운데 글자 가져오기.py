ca63335b08549119406634340a5b236426d7e734
송준호 <1999junho@naver.com>
2023-09-12 14:26:23 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -0,0 +1,8 @@
+def solution(s):
+    if len(s) <= 2:
+        return s
+        
+    if len(s) % 2 == 0:
+        return s[len(s) // 2 - 1 : len(s) // 2 + 1]
+    else:
+        return s[len(s) // 2]
