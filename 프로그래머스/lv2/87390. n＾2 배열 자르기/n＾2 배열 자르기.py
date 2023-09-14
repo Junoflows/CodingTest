bad6fc7455fe72bcd9321cd1bdd9f3d2217570bf
송준호 <1999junho@naver.com>
2023-09-14 12:01:45 +0900

    Update n＾2 배열 자르기.py

--- 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
@@ -1,20 +1,20 @@
 def solution(n, left, right):
-    mid_li = []
-    for i in range(left // n + 1, right // n):
-        li = [i+1 for _ in range(i)] + [j for j in range(i+1,n+1)]  
-        mid_li += li
-    
     if left // n < right // n:
+        mid_li = []
+        for i in range(left // n + 1, right // n):
+            li = [i+1 for _ in range(i)] + [j+1 for j in range(i,n)]  
+            mid_li += li
+        
         idx = left // n
-        left_li = [idx+1 for _ in range(idx)] + [j for j in range(idx+1,n+1)]
+        left_li = [idx+1 for _ in range(idx)] + [j+1 for j in range(idx,n)]
 
         idx = right // n
-        right_li = [idx+1 for _ in range(idx)] + [j for j in range(idx+1,n+1)]
+        right_li = [idx+1 for _ in range(idx)] + [j+1 for j in range(idx,n)]
 
-        return left_li[left % n:] + mid_li + right_li[:right % n +1]
+        return left_li[left % n:] + mid_li + right_li[:right % n + 1]
     
     elif left // n == right // n:
         idx = left // n
-        left_li = [idx+1 for _ in range(idx)] + [j for j in range(idx+1,n+1)]
+        left_li = [idx+1 for _ in range(idx)] + [j+1 for j in range(idx,n)]
         
         return left_li[left % n : right % n + 1]
