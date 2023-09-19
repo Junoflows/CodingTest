047c389f6159ccecead9677273f438cd5ccd3ed7
송준호 <1999junho@naver.com>
2023-09-19 14:11:19 +0900

    [level 1] Title: 행렬의 덧셈, Time: 14.23 ms, Memory: 22.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,19 +1,24 @@
-def solution(arr1, arr2):
-#     li2 = []
-#     length = len(arr1)
+# def solution(arr1, arr2):
+# #     li2 = []
+# #     length = len(arr1)
 
-#     for i in range(len(arr1)):
-#         li = []
-#         for j in range(len(arr1[i])):
-#             li.append(arr1[i][j] + arr2[i][j])
+# #     for i in range(len(arr1)):
+# #         li = []
+# #         for j in range(len(arr1[i])):
+# #             li.append(arr1[i][j] + arr2[i][j])
         
-#         li2.append(li)
+# #         li2.append(li)
     
-#     return li2
-    # zip 을 활용해서 풀기
-    li = []
-    for c,d in zip(arr1, arr2):
-        li.append([c[x]+d[x] for x in range(len(c))])
+# #     return li2
+#     # zip 을 활용해서 풀기
+#     li = []
+#     for c,d in zip(arr1, arr2):
+#         li.append([c[x]+d[x] for x in range(len(c))])
     
-    return li
-    
+#     return li
+def solution(arr1, arr2):
+    result = []
+    for i,j in zip(arr1, arr2):
+        li = [x+y for x,y in zip(i,j)]
+        result.append(li)
+    return result
