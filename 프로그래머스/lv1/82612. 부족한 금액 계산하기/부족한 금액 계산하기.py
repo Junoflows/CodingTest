3f6692042f6569e11e699d0c06a51e6d081ea685
송준호 <1999junho@naver.com>
2023-09-19 14:39:46 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -1,6 +1,12 @@
-def solution(price, money, count):
-    # hap = sum([price*counts for counts in range(1, count+1)])
-    # return hap - money if hap > money else 0
+# def solution(price, money, count):
+#     # hap = sum([price*counts for counts in range(1, count+1)])
+#     # return hap - money if hap > money else 0
     
-    # 등차수열의 합 이용
-    return max(0, price*count*(count+1)//2 - money)
+#     # 등차수열의 합 이용
+#     return max(0, price*count*(count+1)//2 - money)
+def solution(price, money, count):
+    hap = 0
+    for i in range(1, count+1):
+        hap += price*i
+    
+    return max(0, hap - money)
