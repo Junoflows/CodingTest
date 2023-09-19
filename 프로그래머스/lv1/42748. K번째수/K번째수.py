09916db9cbaad1b00ba95a7dc5902c9a4f8cbf15
송준호 <1999junho@naver.com>
2023-09-19 15:36:49 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,8 +1,21 @@
-def solution(array, commands):
-    answer = []
-    for command in commands:
-        li = array[command[0]-1:command[1]]
-        li.sort()
-        answer.append(li[command[2]-1])
+# def solution(array, commands):
+#     answer = []
+#     for command in commands:
+#         li = array[command[0]-1:command[1]]
+#         li.sort()
+#         answer.append(li[command[2]-1])
     
-    return answer
+#     return answer
+
+
+def solution(array, commands):
+    result = []
+    for command in commands:
+        li = sorted([array[i] for i in range(command[0]-1, command[1])])
+        print(li)
+        result.append(li[command[-1]-1])
+        
+    print(result)
+    return result
+        
+        
